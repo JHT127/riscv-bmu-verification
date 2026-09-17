cbf4706fa3f8f52b7194a966366208ca49b15f51
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:06:05 +0300

    add max signed sequence

---
+++ tb/sequences/minmax_ops/max_signed_seq.sv
@@ -0,0 +1,24 @@
+
+// bmu max signed sequence ===========================================
+
+
+class bmu_max_signed_sequence extends bmu_minmax_base_sequence;
+
+        `uvm_object_utils(bmu_max_signed_sequence)
+
+        function new(string name = "bmu_max_signed_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = -32'sd1;
+                req.b_in = 32'd1;
+                req.ap.max = 1'b1;
+                req.ap.sub = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_max_signed_sequence
