6c1f482be075c9718e484210a4f1aab425c3bbce
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:00:37 +0300

    add zba base sequence

---
+++ tb/sequences/zba_ops/bmu_zba_base_sequence.sv
@@ -0,0 +1,19 @@
+
+// bmu zba base sequence ===========================================
+
+
+class bmu_zba_base_sequence extends bmu_base_sequence;
+
+
+        // utility macro ----------------------------------------
+                `uvm_object_utils(bmu_zba_base_sequence)
+
+
+
+        // constructor ----------------------------------------
+        function new(string name = "bmu_zba_base_sequence");
+                super.new(name);
+        endfunction : new
+
+
+endclass : bmu_zba_base_sequence
