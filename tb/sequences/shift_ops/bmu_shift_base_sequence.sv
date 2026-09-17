4c5b0a6b3d5b9907f371e25906251aa553116d82
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:00:25 +0300

    add shift base sequence

---
+++ tb/sequences/shift_ops/bmu_shift_base_sequence.sv
@@ -0,0 +1,19 @@
+
+// bmu shift base sequence ===========================================
+
+
+class bmu_shift_base_sequence extends bmu_base_sequence;
+
+
+        // utility macro ----------------------------------------
+                `uvm_object_utils(bmu_shift_base_sequence)
+
+
+
+        // constructor ----------------------------------------
+        function new(string name = "bmu_shift_base_sequence");
+                super.new(name);
+        endfunction : new
+
+
+endclass : bmu_shift_base_sequence
