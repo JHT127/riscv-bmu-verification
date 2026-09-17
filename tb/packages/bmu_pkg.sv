38e97c5fbac30c80f86d0f0e858fc01bee91dec0
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:01:15 +0300

    include sequence base files

--- tb/packages/bmu_pkg.sv
+++ tb/packages/bmu_pkg.sv
@@ -39,4 +39,22 @@ package bmu_pkg;
         `include "../env/bmu_environment.sv"
 
 
+        // base sequence ----------------------------------------
+        `include "../sequences/base/bmu_base_sequence.sv"
+
+
+        // family base sequences ----------------------------------------
+        `include "../sequences/logic_ops/bmu_logic_base_sequence.sv"
+        `include "../sequences/shift_ops/bmu_shift_base_sequence.sv"
+        `include "../sequences/bit_ops/bmu_bit_ops_base_sequence.sv"
+        `include "../sequences/count_ops/bmu_count_base_sequence.sv"
+        `include "../sequences/sign_extend_ops/bmu_sign_extend_base_sequence.sv"
+        `include "../sequences/minmax_ops/bmu_minmax_base_sequence.sv"
+        `include "../sequences/pack_ops/bmu_pack_base_sequence.sv"
+        `include "../sequences/csr_ops/bmu_csr_base_sequence.sv"
+        `include "../sequences/zba_ops/bmu_zba_base_sequence.sv"
+        `include "../sequences/error_injection/bmu_error_injection_base_sequence.sv"
+        `include "../sequences/reset_ops/bmu_reset_base_sequence.sv"
+
+
 endpackage : bmu_pkg
