67beef9ac5ec58df71e4fa012dd36e6dab897b75
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:04:02 +0300

    add or invert sequence

---
+++ tb/sequences/logic_ops/or_invert_seq.sv
@@ -0,0 +1,24 @@
+
+// bmu or invert sequence ===========================================
+
+
+class bmu_or_invert_sequence extends bmu_logic_base_sequence;
+
+        `uvm_object_utils(bmu_or_invert_sequence)
+
+        function new(string name = "bmu_or_invert_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = 32'hAAAA_AAAA;
+                req.b_in = 32'h5555_5555;
+                req.ap.lor = 1'b1;
+                req.ap.zbb = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_or_invert_sequence
