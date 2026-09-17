0f7e595c083a82bee754ba4c91dbd6aa757be560
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:08:47 +0300

    include all sequences

--- tb/packages/bmu_pkg.sv
+++ tb/packages/bmu_pkg.sv
@@ -57,4 +57,103 @@ package bmu_pkg;
         `include "../sequences/reset_ops/bmu_reset_base_sequence.sv"
 
 
+        // logic sequences ----------------------------------------
+        `include "../sequences/logic_ops/or_valid_seq.sv"
+        `include "../sequences/logic_ops/or_invert_seq.sv"
+        `include "../sequences/logic_ops/xor_valid_seq.sv"
+        `include "../sequences/logic_ops/xor_invert_seq.sv"
+
+
+        // shift sequences ----------------------------------------
+        `include "../sequences/shift_ops/srl_valid_seq.sv"
+        `include "../sequences/shift_ops/srl_corner_seq.sv"
+        `include "../sequences/shift_ops/sra_valid_seq.sv"
+        `include "../sequences/shift_ops/sra_corner_seq.sv"
+        `include "../sequences/shift_ops/ror_valid_seq.sv"
+        `include "../sequences/shift_ops/ror_corner_seq.sv"
+
+
+        // bit operation sequences ----------------------------------------
+        `include "../sequences/bit_ops/binv_valid_seq.sv"
+        `include "../sequences/bit_ops/binv_corner_seq.sv"
+
+
+        // arithmetic and comparison sequences ----------------------------------------
+        `include "../sequences/base/sub_valid_seq.sv"
+        `include "../sequences/base/sub_corner_seq.sv"
+        `include "../sequences/base/slt_signed_seq.sv"
+        `include "../sequences/base/slt_unsigned_seq.sv"
+        `include "../sequences/base/slt_equal_seq.sv"
+
+
+        // count sequences ----------------------------------------
+        `include "../sequences/count_ops/ctz_valid_seq.sv"
+        `include "../sequences/count_ops/ctz_zero_seq.sv"
+        `include "../sequences/count_ops/ctz_ones_seq.sv"
+        `include "../sequences/count_ops/cpop_valid_seq.sv"
+        `include "../sequences/count_ops/cpop_corner_seq.sv"
+
+
+        // sign extension sequences ----------------------------------------
+        `include "../sequences/sign_extend_ops/sextb_pos_seq.sv"
+        `include "../sequences/sign_extend_ops/sextb_neg_seq.sv"
+
+
+        // minmax sequences ----------------------------------------
+        `include "../sequences/minmax_ops/max_valid_seq.sv"
+        `include "../sequences/minmax_ops/max_reversed_seq.sv"
+        `include "../sequences/minmax_ops/max_signed_seq.sv"
+
+
+        // pack sequences ----------------------------------------
+        `include "../sequences/pack_ops/pack_valid_seq.sv"
+        `include "../sequences/pack_ops/pack_corner_seq.sv"
+        `include "../sequences/pack_ops/grev_valid_seq.sv"
+        `include "../sequences/pack_ops/grev_undefined_seq.sv"
+
+
+        // csr sequences ----------------------------------------
+        `include "../sequences/csr_ops/csr_bypass_read_seq.sv"
+        `include "../sequences/csr_ops/csr_write_imm_seq.sv"
+        `include "../sequences/csr_ops/csr_write_reg_seq.sv"
+
+
+        // zba sequences ----------------------------------------
+        `include "../sequences/zba_ops/sh2add_valid_seq.sv"
+        `include "../sequences/zba_ops/sh2add_no_zba_seq.sv"
+
+
+        // error injection sequences ----------------------------------------
+        `include "../sequences/error_injection/or_conflict_seq.sv"
+        `include "../sequences/error_injection/xor_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/srl_conflict_seq.sv"
+        `include "../sequences/error_injection/sra_conflict_seq.sv"
+        `include "../sequences/error_injection/ror_conflict_seq.sv"
+        `include "../sequences/error_injection/binv_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/sh2add_conflict_seq.sv"
+        `include "../sequences/error_injection/sub_zba_misuse_seq.sv"
+        `include "../sequences/error_injection/slt_conflict_seq.sv"
+        `include "../sequences/error_injection/ctz_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/cpop_conflict_seq.sv"
+        `include "../sequences/error_injection/sextb_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/max_conflict_seq.sv"
+        `include "../sequences/error_injection/pack_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/grev_csr_conflict_seq.sv"
+        `include "../sequences/error_injection/random_error_seq.sv"
+
+
+        // reset and timing sequences ----------------------------------------
+        `include "../sequences/reset_ops/latency_check_seq.sv"
+        `include "../sequences/reset_ops/valid_in_hold_seq.sv"
+        `include "../sequences/reset_ops/back_to_back_seq.sv"
+        `include "../sequences/reset_ops/reset_clear_seq.sv"
+        `include "../sequences/reset_ops/reset_mid_op_seq.sv"
+        `include "../sequences/reset_ops/scan_mode_seq.sv"
+
+
+        // random sequences ----------------------------------------
+        `include "../sequences/base/random_legal_seq.sv"
+        `include "../sequences/base/random_corner_weighted_seq.sv"
+
+
 endpackage : bmu_pkg
