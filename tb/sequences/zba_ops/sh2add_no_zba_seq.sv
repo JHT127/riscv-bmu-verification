52a2f2509740cc2408f6217948d5523c6f7b8aba
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:04:54 +0300

    add sh2add guard sequence

---
+++ tb/sequences/zba_ops/sh2add_no_zba_seq.sv
@@ -0,0 +1,23 @@
+
+// bmu sh2add no zba sequence ===========================================
+
+
+class bmu_sh2add_no_zba_sequence extends bmu_zba_base_sequence;
+
+        `uvm_object_utils(bmu_sh2add_no_zba_sequence)
+
+        function new(string name = "bmu_sh2add_no_zba_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = 32'd4;
+                req.b_in = 32'd7;
+                req.ap.sh2add = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_sh2add_no_zba_sequence
