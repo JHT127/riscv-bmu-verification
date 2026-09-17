408f5aadcc660208e9dafaad17f7cdee5f41ff19
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:07:13 +0300

    add srl conflict sequence

---
+++ tb/sequences/error_injection/srl_conflict_seq.sv
@@ -0,0 +1,22 @@
+
+// bmu srl conflict sequence ===========================================
+
+
+class bmu_srl_conflict_sequence extends bmu_error_injection_base_sequence;
+
+        `uvm_object_utils(bmu_srl_conflict_sequence)
+
+        function new(string name = "bmu_srl_conflict_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.ap.srl = 1'b1;
+                req.ap.sra = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_srl_conflict_sequence
