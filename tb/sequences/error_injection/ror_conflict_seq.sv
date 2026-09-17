a77174888dc01dec8daaeb0ff90bed40065949d9
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:07:17 +0300

    add ror conflict sequence

---
+++ tb/sequences/error_injection/ror_conflict_seq.sv
@@ -0,0 +1,22 @@
+
+// bmu ror conflict sequence ===========================================
+
+
+class bmu_ror_conflict_sequence extends bmu_error_injection_base_sequence;
+
+        `uvm_object_utils(bmu_ror_conflict_sequence)
+
+        function new(string name = "bmu_ror_conflict_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.ap.ror = 1'b1;
+                req.ap.binv = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_ror_conflict_sequence
