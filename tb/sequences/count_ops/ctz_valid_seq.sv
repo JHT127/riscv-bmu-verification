47214faeab8b9d92d8c8bb2ee215e15c46c0892a
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:05:48 +0300

    add ctz valid sequence

---
+++ tb/sequences/count_ops/ctz_valid_seq.sv
@@ -0,0 +1,22 @@
+
+// bmu ctz valid sequence ===========================================
+
+
+class bmu_ctz_valid_sequence extends bmu_count_base_sequence;
+
+        `uvm_object_utils(bmu_ctz_valid_sequence)
+
+        function new(string name = "bmu_ctz_valid_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = 32'h0000_0100;
+                req.ap.ctz = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_ctz_valid_sequence
