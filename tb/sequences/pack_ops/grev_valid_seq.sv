4c3912745fa8901f6e74847ffeb0f6b787d0da5b
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:06:10 +0300

    add grev valid sequence

---
+++ tb/sequences/pack_ops/grev_valid_seq.sv
@@ -0,0 +1,23 @@
+
+// bmu grev valid sequence ===========================================
+
+
+class bmu_grev_valid_sequence extends bmu_pack_base_sequence;
+
+        `uvm_object_utils(bmu_grev_valid_sequence)
+
+        function new(string name = "bmu_grev_valid_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = 32'h1234_5678;
+                req.b_in = 32'd24;
+                req.ap.grev = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_grev_valid_sequence
