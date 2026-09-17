c4c8e8506b84f0927306f8048410493c184cd50e
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:06:08 +0300

    add pack corner sequence

---
+++ tb/sequences/pack_ops/pack_corner_seq.sv
@@ -0,0 +1,23 @@
+
+// bmu pack corner sequence ===========================================
+
+
+class bmu_pack_corner_sequence extends bmu_pack_base_sequence;
+
+        `uvm_object_utils(bmu_pack_corner_sequence)
+
+        function new(string name = "bmu_pack_corner_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.a_in = 32'h0000_FFFF;
+                req.b_in = 32'h0000_0000;
+                req.ap.pack = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_pack_corner_sequence
