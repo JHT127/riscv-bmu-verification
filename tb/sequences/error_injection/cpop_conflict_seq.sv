30e2327000de2c068d16029b038abdaa6287689f
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:07:27 +0300

    add cpop conflict sequence

---
+++ tb/sequences/error_injection/cpop_conflict_seq.sv
@@ -0,0 +1,22 @@
+
+// bmu cpop conflict sequence ===========================================
+
+
+class bmu_cpop_conflict_sequence extends bmu_error_injection_base_sequence;
+
+        `uvm_object_utils(bmu_cpop_conflict_sequence)
+
+        function new(string name = "bmu_cpop_conflict_sequence");
+                super.new(name);
+        endfunction : new
+
+        task body();
+                bmu_sequence_item req;
+                req = bmu_sequence_item::type_id::create("req");
+                initialize_item(req);
+                req.ap.cpop = 1'b1;
+                req.ap.pack = 1'b1;
+                send_item(req);
+        endtask : body
+
+endclass : bmu_cpop_conflict_sequence
