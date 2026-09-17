1161b26ee6f73c3665c3d04f193b175992d2d05d
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 13:41:56 +0300

    rename bmu package

---
+++ tb/packages/bmu_pkg.sv
@@ -0,0 +1,16 @@
+
+// bmu package ===========================================
+
+
+package bmu_pkg;
+
+        import uvm_pkg::*;
+        import bmu_types_package::*;
+        `include "uvm_macros.svh"
+
+
+        // sequence item ----------------------------------------
+        `include "../env/agents/bmu_agent/bmu_sequence_item.sv"
+
+
+endpackage : bmu_pkg
