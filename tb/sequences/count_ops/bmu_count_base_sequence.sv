05535155a6306b0b94bb199d2d8fd443d820eb58
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-17 14:00:29 +0300

    add count base sequence

---
+++ tb/sequences/count_ops/bmu_count_base_sequence.sv
@@ -0,0 +1,19 @@
+
+// bmu count base sequence ===========================================
+
+
+class bmu_count_base_sequence extends bmu_base_sequence;
+
+
+        // utility macro ----------------------------------------
+                `uvm_object_utils(bmu_count_base_sequence)
+
+
+
+        // constructor ----------------------------------------
+        function new(string name = "bmu_count_base_sequence");
+                super.new(name);
+        endfunction : new
+
+
+endclass : bmu_count_base_sequence
