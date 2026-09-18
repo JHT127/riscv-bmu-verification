64f58ba1048f4831636a09d9ac27feb50ab5a6ea
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-18 18:27:43 +0300

    fix smoke test syntax

---
+++ tb/tests/bmu_or_valid_test.sv
@@ -0,0 +1,28 @@
+
+// bmu or valid test ===========================================
+
+
+class bmu_or_valid_test extends bmu_base_test;
+
+
+        // utility macro ----------------------------------------
+                `uvm_component_utils(bmu_or_valid_test)
+
+
+
+        // constructor ----------------------------------------
+        function new(string name, uvm_component parent);
+                super.new(name, parent);
+        endfunction : new
+
+
+
+        // run phase ----------------------------------------
+        task run_phase(uvm_phase phase);
+                phase.raise_objection(this);
+                bmu_or_valid_sequence::type_id::create("sequence").start(environment.agent.sequencer);
+                phase.drop_objection(this);
+        endtask : run_phase
+
+
+endclass : bmu_or_valid_test
