dc4def9d51d7c2554dd6a5ca7580c72f5578d630
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-18 15:32:39 +0300

    fix vcs filelist paths

--- sim/filelists/vcs.f
+++ sim/filelists/vcs.f
@@ -2,18 +2,19 @@
 # Fill in once RTL is dropped into ../../rtl/ locally (not committed).
 
 # ----- RTL (design team supplied, not modified) -----
--f ../../rtl/rtl_filelist.f
+-f ../rtl/rtl_filelist.f
 
 # ----- Include directories -----
-+incdir+../../tb/include
-+incdir+../../tb/interface
++incdir+../tb/include
++incdir+../tb/packages
++incdir+../tb/interface
 
 # ----- Packages (compile order matters) -----
-../../tb/packages/bmu_types_package.sv
-../../tb/packages/bmu_pkg.sv
+../tb/packages/bmu_types_package.sv
+../tb/packages/bmu_pkg.sv
 
 # ----- Interface -----
-../../tb/interface/bmu_interface.sv
+../tb/interface/bmu_interface.sv
 
 # ----- Testbench top -----
-../../tb/top/bmu_tb_top.sv
+../tb/top/bmu_tb_top.sv
