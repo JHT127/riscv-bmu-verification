69f0fd04244a9216f0c6283673d8e292910230a0
JHT127 <Trainee11@orionvtech.orionvtech.com>
2026-09-18 15:32:04 +0300

    fix rtl filelist paths

--- rtl/rtl_filelist.f
+++ rtl/rtl_filelist.f
@@ -1,8 +1,8 @@
-+incdir+rtl
-+incdir+rtl/library
++incdir+../rtl
++incdir+../rtl/library
 
-rtl/rtl_defines.sv
-rtl/rtl_pdef.sv
-rtl/rtl_def.sv
-rtl/rtl_lib.sv
-rtl/Bit_Manipulation_Unit.sv
+../rtl/rtl_defines.sv
+../rtl/rtl_pdef.sv
+../rtl/rtl_def.sv
+../rtl/rtl_lib.sv
+../rtl/Bit_Manipulation_Unit.sv
