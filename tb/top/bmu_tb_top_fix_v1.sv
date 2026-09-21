264a694b30d2425bfd639ce7a702c1810f1bd612
JHT127 <jht127@users.noreply.github.com>
2026-09-21 13:11:47 +0300

    add fixed tb top

---
+++ tb/top/bmu_tb_top_fix_v1.sv
@@ -0,0 +1,48 @@
+
+// fixed RTL testbench top ===========================================
+
+import uvm_pkg::*;
+import bmu_pkg::*;
+
+
+module bmu_tb_top_fix_v1;
+
+        logic clk;
+        bmu_interface bmu_if(clk);
+
+        initial begin
+                clk = 1'b0;
+                forever #5 clk <= ~clk;
+        end
+
+        initial begin
+                bmu_if.rst_l = 1'b0;
+                bmu_if.scan_mode = 1'b0;
+                bmu_if.valid_in = 1'b0;
+                bmu_if.a_in = '0;
+                bmu_if.b_in = '0;
+                bmu_if.ap = '0;
+                bmu_if.csr_ren_in = 1'b0;
+                bmu_if.csr_rddata_in = '0;
+        end
+
+        Bit_Manipulation_Unit_fix_v1 dut (
+                .clk           (clk),
+                .rst_l         (bmu_if.rst_l),
+                .scan_mode     (bmu_if.scan_mode),
+                .valid_in      (bmu_if.valid_in),
+                .ap            (bmu_if.dut_ap),
+                .csr_ren_in    (bmu_if.csr_ren_in),
+                .csr_rddata_in (bmu_if.csr_rddata_in),
+                .a_in          (bmu_if.a_in),
+                .b_in          (bmu_if.b_in),
+                .result_ff     (bmu_if.result_ff),
+                .error         (bmu_if.error)
+        );
+
+        initial begin
+                uvm_config_db #(virtual bmu_interface)::set(null, "*", "vif", bmu_if);
+                run_test();
+        end
+
+endmodule : bmu_tb_top_fix_v1
